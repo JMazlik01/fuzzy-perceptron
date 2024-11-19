b0a74b3b99c1088e4b7640b75b0f6e90b3beb190
JMazlik01 <jakub.mazel@icloud.com>
2024-11-19 23:01:03 +0100

    Rewritten fuzzy partition to array representation

--- src/fuzzy-perceptron.lisp
+++ src/fuzzy-perceptron.lisp
@@ -4,53 +4,60 @@
 
 ;;; FUZZY PARTITION
 
-(defun partition-function (d1 d2 d L &optional class2)
-  (let ((u (+ 0.5
-	      (/ (- (exp (/ (* L (- d2 d1))
-			    d))
-		    (exp (- L)))
-		 (* 2 (- (exp L) (exp (- L))))))))
-    (if class2
-	(list (- 1 u) u)
-	(list u (- 1 u)))))
+;; partition function from original article
+;; class 1 and class 2 elements differ in d1 and d2 order
+(defun partition-function (d1 d2 d L)
+  (+ 0.5
+     (/ (- (exp (/ (* L (- d2 d1))
+		   d))
+	   (exp (- L)))
+	(* 2 (- (exp L) (exp (- L)))))))
 
-(defun class1partition (d L)
-  (lambda (d1 d2)
-    (partition-function d1 d2 d L)))
-
-(defun class2partition (d L)
-  (lambda (d1 d2)
-    (partition-function d2 d1 d L t)))
 
 (defun class-distance (class1 class2)
   (distance (mean-vector class1)
 	    (mean-vector class2)))
 
-(defun class-partition (m1 m2 class c)
-  (mapcar (lambda (dist)
-	    (funcall c (first dist) (second dist)))
-	  (mapcar (lambda (v)
-		    (list (distance m1 v)
-			  (distance m2 v)))
-		  class)))
+(defun empty-partition (class1 class2)
+  (make-array (+ (length class1)
+		 (length class2))
+	      :initial-element 0))
 
-(defun u (matrix i k)
-  (if (< 1 k)
-      (u (cdr matrix) i (1- k))
-      (if (= i 1)
-	  (first (car matrix))
-	  (second (car matrix)))))
+(defun class-partition (class par-fun)
+  (let ((part (make-array (length class)
+			  :initial-element 0)))
+    (loop
+      for v in class
+      for i = 0 then (1+ i)
+      do (setf (elt part i)
+	       (funcall par-fun v)))
+    part))
+			  
+(defun fuzzy-partition (class1 class2 L)
+  (let ((m1 (mean-vector class1))
+	(m2 (mean-vector class2))
+	(d (class-distance class1 class2)))
+    (concatenate
+     'vector
+     (class-partition
+      class1
+      (lambda (v)
+	(partition-function (distance v m1)
+			    (distance v m2)
+			    d
+			    L)))
+     (class-partition
+      class2
+      (lambda (v)
+	(- 1  (partition-function (distance v m2)
+				  (distance v m1)
+				  d
+				  L)))))))
 
-;;; PREPSAT NA 1D POLE? BUDE TO RYCHLEJSI
-(defun partition-matrix (class1 class2 L)
-  (let* ((mean1 (mean-vector class1))
-	 (mean2 (mean-vector class2))
-	 (d (distance mean1 mean2))
-	 (c1 (class1partition d L))
-	 (c2 (class2partition d L)))
-    (append
-     (class-partition mean1 mean2 class1 c1)
-     (class-partition mean1 mean2 class2 c2))))
+(defun part-elem (matrix i k)
+  (if (= i 1)
+      (elt matrix k)
+      (- 1 (elt matrix k))))
 
 
 ;;; FUZZY PERCEPTRON ALGORITHM
@@ -69,24 +76,24 @@
 	      (not (<= (- 0.5 beta) u1 (+ 0.5 beta))))
       (values W nil)))
 
-(defun fuzzy-update (ts W u c m beta &optional (updatep nil))
+(defun fuzzy-update (ts W u c c-i m beta &optional (updatep nil))
   (if (null ts)
       (values W updatep)
       (multiple-value-bind (nW update)
-	  (fuzzy-weight-update W (first ts) (u u 1 1) (u u 2 1) c m beta)
-	(fuzzy-update (cdr ts) nW (cdr u) c m beta (or updatep update)))))
+	  (fuzzy-weight-update W (first ts) (part-elem u 1 c-i) (part-elem u 2 c-i) c m beta)
+	(fuzzy-update (cdr ts) nW u c (1+ c-i) m beta (or updatep update)))))
 
 (defun fuzzy-perceptron-algorithm (class1 class2 partition W c m beta &key (bound 2000))
   (labels ((fuzzy-perceptron-algorithm-h (ts partition W c m beta count)
 	     (multiple-value-bind (nW update)
-		 (fuzzy-update ts W partition c m beta)
+		 (fuzzy-update ts W partition c 0 m beta)
 	       (if (and update (< count bound))
 		   (fuzzy-perceptron-algorithm-h ts partition nW c m beta (1+ count))
 		   (values  nW count)))))
     (fuzzy-perceptron-algorithm-h (initialize-classes class1 class2) partition W c m beta 0)))
 			
 (defun train-fuzzy-perceptron (class1 class2 &key (L 2.5) (m 1.2) (c 0.1) (eps 0.02))
-  (let* ((partition (partition-matrix class1 class2 L))
+  (let* ((partition (fuzzy-partition class1 class2 L))
 	 (beta (beta L eps)))
     (multiple-value-bind (w count)
 	(fuzzy-perceptron-algorithm class1 class2 partition (make-n-dim-vector (class-dim class1)) c m beta)
